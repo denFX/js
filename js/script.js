f7dd2d3c4b43034b3717c0ec43f2b8905412e2e2
Denis <den.@mail.ru>
2024-05-23 13:59:30 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const answer = [];
+
+answer [0] = prompt("Wie alt bist du?")
+answer [1] = prompt("Name?")
+answer [2] = prompt("Nachname?")
+answer [3] = prompt("Wie lang is bei dir?", "20")
+
+console.log (answer[1]);
