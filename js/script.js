b7992fb8d99660b7e0b618667d8df529b6a71c1d
Denis <denlsdkfj@mail.ru>
2024-05-28 18:39:18 +0200

    fragen zur filme hinzugefügt

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-const answer = [];
+//const answer = [];
 
-answer [0] = prompt("Wie alt bist du?")
-answer [1] = prompt("Name?")
-answer [2] = prompt("Nachname?")
-answer [3] = prompt("Wie lang is bei dir?", "20")
+//answer [0] = prompt("Wie alt bist du?")
+//answer [1] = prompt("Name?")
+//answer [2] = prompt("Nachname?")
+//answer [3] = prompt("Wie lang is bei dir?", "20")
 
-console.log (answer[1]);
+//console.log (answer[1]);
 
-<h2>Hello word!</h2>
+//<h2>Hello word!</h2>
+
+let numberOfFims = +prompt ("How much vovies du you watcht?", " ");
+
+const personalMovieDB = {
+count: numberOfFims,
+movies: {},
+actors: {},
+genres: [],
+private: false
+};
+
+const   a = prompt("Last Film", ""),
+        b = prompt("woh well", ""),
+        c = prompt("last film", ""),
+        d = prompt("how well", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
