9b34809f145e73479840c1ff895e63a3b3830cdc
Denis <den.@mail.ru>
2024-06-27 23:09:30 +0200

    dell all from script.js for praxis

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
-const answer = [];
+"use strict";
 
-answer [0] = prompt("Wie alt bist du?")
-answer [1] = prompt("Name?")
-answer [2] = prompt("Nachname?")
-answer [3] = prompt("Wie lang is bei dir?", "20")
-
-console.log (answer[1]);
-
-<h2>Hello word!</h2>
